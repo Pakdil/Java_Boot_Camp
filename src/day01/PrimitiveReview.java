2848387cdbb92fabd594aa6a99bcfb12e0f07396
pakdil <eaasbb@gmail.com>
2022-12-13 23:25:39 +0300

    Java Boot Camp day01 PrimitiveReview

---
+++ src/day01/PrimitiveReview.java
@@ -0,0 +1,31 @@
+package day01;
+
+public class PrimitiveReview {
+
+    public static void main(String[] args) {
+
+        // Data-Type  var;
+
+        int age = 10;
+        double tax = 2.5;
+        char ch = '$';
+        boolean r = true;
+
+        int ch1 =  ch;
+        System.out.println("ch1 = " + ch1);
+        System.out.println("ch = " + ch);
+
+        int b = 100;
+        double c =  b; // implicit casting
+
+
+        double x = 20.5;
+        int y = (int) x;  // explicit casting
+
+
+
+
+
+
+    }
+}
