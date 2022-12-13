697dfe8f0dcae1fca08a41d858fb5093a271054e
pakdil <eaasbb@gmail.com>
2022-12-13 19:28:12 +0300

    Java Boot Camp day01

--- src/day01/SequenceOfCharacters.java
+++ src/day01/SequenceOfCharacters.java
@@ -1,2 +1,39 @@
-package day01;public class SequenceOfCharacters {
+package day01;
+
+public class SequenceOfCharacters {
+
+    public static void main(String[] args) {
+
+    String str1 = "cydeo";
+
+    String str2 = "cydeo";
+
+        String str3 = new String("cydeo");
+        String str4 = new String("cydeo");
+
+        String str5 = str1.toUpperCase(); // "CYDEO"
+
+        System.out.println(str5);
+
+        str2 = str2.concat("School");
+
+        System.out.println(str2);
+
+        // StringBuilder stringBuilder = "cydeo";
+        //  StringBuffer stringBuffer = "cydeo";
+
+        StringBuffer stringBuffer = new StringBuffer("Cydeo");
+
+        stringBuffer.append(" School");
+
+        System.out.println(stringBuffer);
+
+
+        StringBuilder stringBuilder = new StringBuilder("Cydeo");
+
+        stringBuilder.append(" School");
+
+        System.out.println(stringBuilder);
+
+    }
 }
--- src/day01/WrapperClasses.java
+++ src/day01/WrapperClasses.java
@@ -1,2 +1,28 @@
-package day01;public class WrapperClasses {
+package day01;
+
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        byte b = 28;
+
+        // Integer a2 = b;
+        Integer x = a;
+        Byte y = b;
+
+        char ch = '$';
+
+        Character z = ch;
+
+
+        System.out.println("---------------------------------------");
+
+        int q =Integer.parseInt("123");
+
+        Integer w =Integer.valueOf("123");
+        
+
+
+    }
 }
