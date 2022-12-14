9dc2de501e3e4d2e185a72b6d2748b291f1ecc05
Pakdil <eaasbb@gmail.com>
2022-12-14 20:18:40 +0300

    day02 abstraction

---
+++ src/day02/abstraction_review/Car.java
@@ -0,0 +1,16 @@
+package day02.abstraction_review;
+
+public abstract class Car {
+
+    public String brand, model;
+
+    public abstract void driver();
+
+    public abstract void start();
+
+    public abstract void stop();
+
+
+}
+
+
---
+++ src/day02/abstraction_review/Tesla.java
@@ -0,0 +1,19 @@
+package day02.abstraction_review;
+
+public class Tesla extends Car{
+
+    @Override
+    public void driver() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
