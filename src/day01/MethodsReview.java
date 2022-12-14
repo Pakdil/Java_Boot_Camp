984a01f693116ea4aa72918a2c3d2258052fddfe
pakdil <eaasbb@gmail.com>
2022-12-14 13:19:46 +0300

    Java Boot Camp day01 MethodsReview update

--- src/day01/MethodsReview.java
+++ src/day01/MethodsReview.java
@@ -2,7 +2,7 @@ package day01;
 
 public class MethodsReview {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // main method can be overloaded
 
         String str1 = "Cydeo";
         printEach(str1);
@@ -22,13 +22,42 @@ public class MethodsReview {
         String str3 = "WoodenSpoon";
         printEach(str3);
 
+        System.out.println("-------------------------------");
 
+        int [] numbers = {10, 20, 30, 40};
+        printEach(numbers);
+        printEach(numbers);
     }
 
     public static void printEach(String str) {
         for (int i = 0; i < str.length(); i++) {
             System.out.println(str.charAt(i));
         }
+    }
+
+        public static void printEach(int[] arr) {
+            for (int each : arr) {
+                System.out.println(each);
+            }
+        }
+
+    public static void printEach(double[] arr) {
+        for (double each : arr) {
+            System.out.println(each);
+        }
+    }
+
+    public static void printEach(char[] arr) {
+        for (char each : arr) {
+            System.out.println(each);
+        }
+    }
+
+    public static void printEach(String[] arr) {
+        for (String each : arr) {
+            System.out.println(each);
+        }
+    }
 
     }
-}
+
